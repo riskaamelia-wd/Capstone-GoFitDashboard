9170bf51a07fc4e18c236ac8dbc861ae7fa05c36
riskaamelia-wd <riskaamelia294@gmail.com>
2023-05-21 19:01:49 +0700

    update : slicer with thunk

--- src/redux/Slice/paymentMethodsSlice.jsx
+++ src/redux/Slice/paymentMethodsSlice.jsx
@@ -1,26 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
+import React from 'react'
+import { v4 as uuid } from 'uuid';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [] 
+const url = 'https://localhost:8000/payment_method'
 
-const paymentMethodsSlice = createSlice({
-    name:'paymentMethods',
-    initialState,
-    reducers:{
-        addPaymentMethodsFromApi: (state, action) => {
-            return (state = action.payload)
-        },
-        addPaymentMethods : (state, action) => {
-            return [...state, action.payload]
-        },
-        deletePaymentMethods : (state, action) => {
-            return state.filter((paymentMethods) => paymentMethods.id !== action.payload)
-        },
-        editPaymentMethods: (state, action) => {
-            return state.filter((paymentMethods) => paymentMethods.id !== action.payload)
+export const getPayment = createAsyncThunk(
+    'payment_method', async () => {
+    const mockApi = url;
+    const fetchData = await axios({
+        method : 'GET',
+        url : mockApi,
+        headers : {
+            'Content-Type' : 'application/json'
         }
+    })
+
+    .then( res => {
+        if (res.status !== 200){
+            console.error("Error, no fetched data");
+            return {}
+        } else {
+            console.log('response => ', res.data)
+            return res.data;
+        }
+    })
+    .catch(error => {
+        console.log('error fetch ', error);
+    })
+    return fetchData
+})
+
+
+const initialState = {
+    data : []
+}
+
+const paymentMethodSlice = createSlice({
+    name : 'payment_method',
+    initialState : initialState,
+    reducers : {},
+
+    extraReducers :  {
+
+        [getPayment.fulfilled] : (state, action) => {
+            return{
+                ...state,
+                data : {
+                    data : action.payload
+                }
+            }
+        }
+
     }
 })
 
-export const {addPaymentMethods, addPaymentMethodsFromApi, deletePaymentMethods, editPaymentMethods} = paymentMethodsSlice.actions;
 
-export default paymentMethodsSlice.reducer;
+export default paymentMethodSlice.reducer
--- src/redux/Slice/usersSlice.jsx
+++ src/redux/Slice/usersSlice.jsx
@@ -1,30 +1,162 @@
-import { createSlice } from "@reduxjs/toolkit";
+import React from 'react'
+import { v4 as uuid } from 'uuid';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-const initialState = [] 
-// {    allUsers: []}
+const url = 'https://localhost:8000/users'
+
+export const getUsers = createAsyncThunk(
+    'users/getUsers', async () => {
+    const mockApi = url;
+    const fetchData = await axios({
+        method : 'GET',
+        url : mockApi,
+        headers : {
+            'Content-Type' : 'application/json'
+        }
+    })
+
+    .then( res => {
+        if (res.status !== 200){
+            console.error("Error, no fetched data");
+            return {}
+        } else {
+            console.log('response => ', res.data)
+            return res.data;
+        }
+    })
+    .catch(error => {
+        console.log('error fetch ', error);
+    })
+    return fetchData
+})
+
+export const addUser = createAsyncThunk('users/addUser',
+    async(param) => {
+        const response = await axios.post(url, param)
+
+        .then( res => {
+            if (res.status !== 200){
+                alert('Data Berhasil di Tambah')
+                console.error("Error, no fetched data");
+                return {}
+            } else {
+                console.log('response addUsers => ', res.data)
+                return res.data;
+            }
+        })
+        .catch(error => {
+            console.log('error fetch ', error);
+        })
+
+        return response.data
+
+
+    }
+)
+
+export const deleteUser = createAsyncThunk(
+    'users/deleteUser', async (id) => {
+    const mockApi = `${url}/${id}`
+        const res = await axios.delete(
+            mockApi
+        )
+
+        .then( res => {
+            if (res.status !== 200){
+                console.error("Error, no fetched data");
+                return {}
+            } else {
+                alert('Data Berhasil di Hapus')
+                console.log('response delete => ', res.data)
+                return res.data;
+            }
+        })
+        .catch(error => {
+            console.log('error fetch ', error);
+        })
+
+        return res.data
+})
+
+export const editUser = createAsyncThunk(
+    'users/editUser', async ([id, data]) => {
+        const res = await axios.put(
+            `${url}/${id}`, {data}
+        )
+
+        .then( res => {
+            if (res.status !== 200){
+                console.error("Error, no fetched data");
+                return {}
+            } else {
+                console.log('response edit => ', res.data)
+                return res.data;
+            }
+        })
+        .catch(error => {
+            console.log('error fetch ', error);
+        })
+
+        return res.data
+})
+
+
+
+const initialState = {
+    data : [],
+    type : '',
+}
 
 const usersSlice = createSlice({
-    name:'users',
-    initialState,
-    reducers:{
-        addUsersFromApi: (state, action) => {
-            // if(state.allUsers.length === 0){
-            //     state.allUsers = action.payload
-            // }
-            return (state = action.payload)
+    name : 'users',
+    initialState : initialState,
+    reducers : {},
+
+    extraReducers :  {
+
+        [getUsers.fulfilled] : (state, action) => {
+            return{
+                ...state,
+                data : {
+                    data : action.payload
+                },
+                type : action.type
+            }
         },
-        addUsers : (state, action) => {
-            return [...state, action.payload]
+
+        [addUser.fulfilled] : ( state, action ) => {
+            return {
+                ...state,
+                type: action.type
+            }
         },
-        deleteUsers : (state, action) => {
-            return state.filter((user) => user.id !== action.payload)
+
+        [deleteUser.fulfilled] : (state, action) => {
+            return{
+                ...state,
+                data : state.data.filter((user) =>
+                user.id !== action.payload.id,
+                ),
+                type : action.type
+            }
+            
         },
-        editUsers: (state, action) => {
-            return state.filter((user) => user.id !== action.payload)
+
+        [editUser.fulfilled.type] : (state,action) => {
+            return{
+                ...state,
+                data : state.data.map((product) => {
+                    if (product.id === action.payload.id) {
+                      product.users = action.payload.users;
+                    }
+                }),
+                type : action.type
+            }
         }
+
     }
 })
 
-export const {addUsers, addUsersFromApi, deleteUsers, editUsers} = usersSlice.actions;
 
-export default usersSlice.reducer;
+export default usersSlice.reducer
