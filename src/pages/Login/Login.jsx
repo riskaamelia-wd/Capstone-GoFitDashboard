e30eec81378b033179d10fa163610872856513cf
neizshya <nurul.adzan24@gmail.com>
2023-05-23 09:58:06 +0700

    add : css active for button

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -21,62 +21,61 @@ const Login = () => {
           </div>
           <div className=" col-md-6 LoginRightSide rounded-5 d-flex justify-content-center align-items-center ">
             <div className=" w-75 ">
-              <form
-              //  action=""
-              >
-                <p className=" mt-5 fw-bold fs-1 h1-rightside">Welcome!</p>
-                <p className=" mt-3 fw-semibold fs-5 mb-4">
-                  Log in you account
-                </p>
-                <TextField
-                  label={`Your Email`}
-                  type={"text"}
-                  name={"email"}
-                  id={"email"}
-                  onChange={() => {}}
+              <p className=" mt-5 fw-bold fs-1 h1-rightside">Welcome!</p>
+              <p className=" mt-3 fw-semibold fs-5 mb-4">Log in you account</p>
+              <TextField
+                label={`Your Email`}
+                type={"text"}
+                name={"email"}
+                id={"email"}
+                onChange={() => {}}
+                // value={}
+                placeholder={"userName@gmail.com"}
+                classNameLabel={"mb-2 text-secondary"}
+              />
+
+              <div className="mt-4">
+                <TextFieldPassword
+                  placeholder={"******"}
+                  label="Password"
+                  id={"password"}
+                  name={"password"}
                   // value={}
-                  placeholder={"userName@gmail.com"}
+                  onChange={() => {}}
                   classNameLabel={"mb-2 text-secondary"}
                 />
-
-                <div className="mt-4">
-                  <TextFieldPassword
-                    placeholder={"******"}
-                    label="Password"
-                    id={"password"}
-                    name={"password"}
-                    // value={}
-                    onChange={() => {}}
-                    classNameLabel={"mb-2 text-secondary"}
-                  />
-                </div>
-                <div className="row mt-3">
-                  <div className="col-6">
-                    <div className="form-check">
-                      <input
-                        type="checkbox"
-                        className="form-check-input"
-                        id="rememberme"
-                        checked={rememberMe}
-                        onChange={() => setRememberMe(!rememberMe)}
-                      />
-                      <label
-                        className="form-check-label fw-semibold"
-                        htmlFor="exampleCheck1">
-                        Remember Me
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-6 text-end">
-                    {/* <NavLink to={""}> */}
-                    <p className="ForgotPasswordText">Forgot Password?</p>
-                    {/* </NavLink> */}
+              </div>
+              <div className="row mt-3">
+                <div className="col-6">
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      id="rememberme"
+                      checked={rememberMe}
+                      onChange={() => setRememberMe(!rememberMe)}
+                    />
+                    <label
+                      className="form-check-label fw-semibold"
+                      htmlFor="exampleCheck1">
+                      Remember Me
+                    </label>
                   </div>
                 </div>
-                <button className="btn w-100 btn-login fs-5" type="submit">
-                  Log in
-                </button>
-              </form>
+                <div className="col-6 text-end">
+                  {/* <NavLink to={""}> */}
+                  <p className="ForgotPasswordText">Forgot Password?</p>
+                  {/* </NavLink> */}
+                </div>
+              </div>
+              <button
+                className="w-100 btn-login fs-5"
+                type="submit"
+                onClick={() => {
+                  console.log("logging in");
+                }}>
+                Log in
+              </button>
               <div className="LineTextMiddle mt-5 mb-5">
                 <span className="fs-5">or</span>
               </div>
