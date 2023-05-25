4c8cc85ef75002cf5680595c60ea4abca7bbc717
Nindya Putri Maharani <nindyaputrimr@gmail.com>
2023-05-25 13:50:50 +0700

    add: searchTopBar.jsx

---
+++ src/elements/InputSearch/SearchTopBar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import '../InputSearch/InputSearch.css';
+import search from "../../assets/icons/search.svg";
+
+const SearchTopBar = ({ type, name, id, onChange, value, placeholder }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleIconClick = () => {
+    setIsVisible(true);
+  };
+
+  return (
+    <>
+      <div className="search-container">
+        <img src={search} className='icon-search' onClick={handleIconClick} />
+        {isVisible && (
+          <input
+            type={type}
+            name={name}
+            id={id}
+            placeholder={placeholder}
+            className="search"
+            onChange={onChange}
+            value={value}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default InputSearch;
