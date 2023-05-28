14d10d4502f83dba2ccb3c0e8a084b6a84081654
Nindya Putri Maharani <nindyaputrimr@gmail.com>
2023-05-28 21:53:09 +0700

    Add: Manage Customers

---
+++ src/pages/ManagesCustomers/ManageCustomers.jsx
@@ -0,0 +1,25 @@
+import Cover from "../../elements/Card/Cover"
+import imgCover from '../../assets/icons/Appreciation 1.svg'
+//Belum Selesai
+const ManageCustomers = () => {
+    return (
+        <>
+        <div className="container">
+            <Cover 
+                text={'Manage Customers'}
+                list1={'Home'}
+                img={imgCover}
+            />
+            <div className="col-5">
+
+            </div>
+            <div className="col-7">
+
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default ManageCustomers
