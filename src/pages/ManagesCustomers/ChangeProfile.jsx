23488df5be4b60fb459791a1045709cd478b2f03
Nindya Putri Maharani <nindyaputrimr@gmail.com>
2023-05-31 10:49:43 +0700

    update: manage customers

---
+++ src/pages/ManagesCustomers/ChangeProfile.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+const [profile, setProfile] = useState([
+    {
+        id: 1,
+        image: "https://source.unsplash.com/random/?profile",
+        name: "Mr. Jack",
+        email: "jack@gmail.com",
+        phone: 085377777777,
+        address: "814 Howard Street, 120065, Indonesia"
+    },
+])
+const ChangeProfile = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default ChangeProfile
