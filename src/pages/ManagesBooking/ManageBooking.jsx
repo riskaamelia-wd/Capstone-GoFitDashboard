14903091b1052e92699a767fa337f69d430cd8c3
Ryzal Fadhillah <fadhillah.ryzal@gmail.com>
2023-06-21 21:01:43 +0700

    update: revisi desain v3

--- src/pages/ManagesBooking/ManageBooking.jsx
+++ src/pages/ManagesBooking/ManageBooking.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import IconMember from '../../assets/icons/membership.svg'
 import ButtonComponent from "../../elements/Buttons/ButtonComponent";
@@ -6,26 +5,17 @@ import axios from "axios";
 import Cover from "../../elements/Card/Cover";
 import { useNavigate } from "react-router";
 import { useSelector } from "react-redux";
-// import useAxios from "../../api/useAxios"
-// import { adminApi } from "../../api/Api"
+import { Button } from "react-bootstrap";
 
 const ManageBooking = () => {
     const navigate = useNavigate();
     const token = useSelector((state) => state.tokenAuth.token_jwt)
     const [datas, setdatas] = useState([]);
     const [viewDetail, setViewDetail] = useState({});
-
-    // const { response, isLoading } = useAxios({
-    //     api: adminApi,
-    //     method: "get",
-    //     url: `/classes/ticket`,
-    //     body: JSON.stringify({}),
-    //     header: JSON.stringify({
-    //         Authorization: `Bearer ${token}`,
-    //     }),
-    // });
+    const [selectedButton, setSelectedButton] = useState('online');
     
     useEffect(() => {
+        //get semua data booking
         axios.get('http://18.141.56.154:8000/admin/classes/tickets',{
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -41,15 +31,18 @@ const ManageBooking = () => {
     },[token])
 
     const handleClick = (id) => {
+        //untuk menampilkan tombol view detail
         setViewDetail((prevState) => ({
-        ...prevState,
-        [id]: !prevState[id],
+            ...prevState,
+            [id]: !prevState[id],
         }));
     };
 
     const handleViewDetail = (id) => {
         navigate(`/booking/detail/${id}`)
     }
+
+    //untuk convert date dari bentuk dd/mm/yyyy ke mount date
     function formatDate(dateString) {
         const date = new Date(dateString);
         const options = { month: 'long', day: 'numeric' };
@@ -57,6 +50,7 @@ const ManageBooking = () => {
         return formattedDate;
     }
 
+    //untuk convert waktu dari 24 jam ke AM/PM
     function formatTime(dateString) {
         const date = new Date(dateString);
         const options = { hour: 'numeric', minute: 'numeric'};
@@ -64,6 +58,13 @@ const ManageBooking = () => {
         return formattedTime;
     }
 
+    const handleButtonClick = (status) => {
+        setSelectedButton(status);
+    };
+
+    //untuk filter data yg ditampilkan online/offline
+    const filteredDatas = datas.filter(data => data.class_package.class.class_type === selectedButton);
+    console.log(filteredDatas.length);
     return (
         <>
         <Cover
@@ -74,7 +75,25 @@ const ManageBooking = () => {
         <div className="" style={{ padding: "42px" }}>
             <div className="title mb-4">
                 <h3 style={{ color: "var(--primary-900)" }}>General Customer</h3>
-                <h5>Here General Customer List Data</h5>
+                <div className="d-flex justify-content-between">
+                    <h5>Here General Customer List Data</h5>
+                    <div>
+                        <Button
+                            className='me-2 shadow'
+                            onClick={() => handleButtonClick('online')}
+                            style={{ backgroundColor: selectedButton === 'online' ? 'var(--Warning-500)' : 'white', color: 'var(--Warning-900)', border: 'none' }}
+                        >
+                            Online
+                        </Button>
+                        <Button
+                            className="shadow"
+                            onClick={() => handleButtonClick('offline')}
+                            style={{ backgroundColor: selectedButton === 'offline' ? 'var(--Warning-500)' : 'white', color: 'var(--Warning-900)', border: 'none' }}
+                        >
+                            Offline
+                        </Button>
+                    </div>
+                </div>
             </div>
             <table className="table table-borderless">
                 <thead
@@ -105,49 +124,47 @@ const ManageBooking = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {datas.length > 0 ? (
-                    datas.map((data) => {
-                        return (
-                        <tr key={data.id}>
-                            <td style={{ width: '40px' }}>
-                                <div
-                                style={{
-                                    borderRadius: '50%',
-                                    backgroundColor: 'var(--primary-100)',
-                                    width: '24px',
-                                    height: '24px',
-                                    margin: 'auto',
-                                }}
-                                ></div>
-                            </td>
-                            <th>{data.user.name}</th>
-                            <td>{data.class_package.class.class_type}</td>
-                            <td>{formatDate(data.class_package.class.started_at)}, {formatTime(data.class_package.class.started_at)}</td>
-                            <td>{data.class_package.price}/{data.class_package.period}</td>
-                            {
-                                data.class_package.class.class_type === 'online' ? 
-                                    <td>Via Zoom</td>:
-                                    <td>{data.class_package.class.location.address}, {data.class_package.class.location.city}</td>
-                            }
-                            <td className="d-flex">
-                                <ButtonComponent
-                                    onClick={() => handleClick(data.id)}
-                                    className={"border-0 bg-transparent text-bold"}
-                                    buttonName={'...'}
-                                />
-                                {viewDetail[data.id] && (
-                                    <button
-                                        style={{color: "var(--info-600)", backgroundColor: "white"}}
-                                        onClick={() => handleViewDetail(data.id)}
-                                        className="shadow border-0 rounded-3"
+                    {filteredDatas.length > 0 ? (
+                        filteredDatas.map((data) => (
+                            <tr key={data.id}>
+                                <td style={{ width: '40px' }}>
+                                    <div
+                                        style={{
+                                            borderRadius: '50%',
+                                            backgroundColor: 'var(--primary-100)',
+                                            width: '24px',
+                                            height: '24px',
+                                            margin: 'auto',
+                                        }}
+                                    ></div>
+                                </td>
+                                <th>{data.user.name}</th>
+                                {
+                                    data.class_package.class.class_type === 'online' ? 
+                                    <td>Via zoom</td>:
+                                    <td>{data.class_package.class.name}</td>
+                                }
+                                <td>{formatDate(data.class_package.class.started_at)}, {formatTime(data.class_package.class.started_at)}</td>
+                                <td>{data.class_package.price}/{data.class_package.period}</td>
+                                <td>{data.class_package.class.location.address}</td>
+                                <td className="d-flex">
+                                    <ButtonComponent
+                                        onClick={() => handleClick(data.id)}
+                                        className={"border-0 bg-transparent text-bold"}
+                                        buttonName={'...'}
+                                    />
+                                    {viewDetail[data.id] && (
+                                        <button
+                                            style={{color: "var(--info-600)", backgroundColor: "white"}}
+                                            onClick={() => handleViewDetail(data.id)}
+                                            className="shadow border-0 rounded-3"
                                         >
-                                        View Detail
-                                    </button>
-                                )}
-                            </td>
-                        </tr>
-                        );
-                    })
+                                            View Detail
+                                        </button>
+                                    )}
+                                </td>
+                            </tr>
+                        ))
                     ) : (
                         <tr>
                             <td colSpan="7" className="text-center">Tidak ada data booking</td>
--- src/pages/ManagesBooking/ManageBookingDetail.jsx
+++ src/pages/ManagesBooking/ManageBookingDetail.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 // import React from 'react'
-import { Col, Row } from 'react-bootstrap'
+import { Button, Col, Row } from 'react-bootstrap'
 import DetailBooking from '../../components/DetailBooking/DetailBooking'
 import CardCustomerBooking from '../../components/CardCustomerBooking/CardCustomerBooking'
 import { useEffect, useState } from 'react'
@@ -14,10 +14,11 @@ const ManageBookingDetail = () => {
     const { id } = useParams();
     const [customers, setCustomers] = useState([]);
     const [booking, setBooking] = useState({})
-    const [idBooking, setIdBooking] = useState('')
     const token = useSelector((state) => state.tokenAuth.token_jwt)
+    const [selectedButton, setSelectedButton] = useState('online');
 
     useEffect(() => {
+        //get data booking untuk ditampilkan di customers data
         axios.get('http://18.141.56.154:8000/admin/classes/tickets',{
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -31,6 +32,7 @@ const ManageBookingDetail = () => {
             console.log(error);
         })
 
+        //untuk get detail booking dari id yg dikirimkan di param
         axios.get(`http://18.141.56.154:8000/admin/classes/tickets/${id}`,{
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -43,8 +45,9 @@ const ManageBookingDetail = () => {
         .catch((error) => {
             console.log(error);
         })
-    },[id])
+    },[id, token])
 
+    //untuk convert date dari bentuk dd/mm/yyyy ke mount date, year
     function formatDate(dateString, type) {
         const date = new Date(dateString);
         const options = {year:'numeric', month: 'long', day: 'numeric' };
@@ -52,6 +55,7 @@ const ManageBookingDetail = () => {
         return formattedDate;
     }
 
+    //untuk convert waktu dari 24 jam ke AM/PM
     function formatTime(dateString) {
         const date = new Date(dateString);
         const options = { hour: 'numeric', minute: 'numeric'};
@@ -59,6 +63,7 @@ const ManageBookingDetail = () => {
         return formattedTime;
     }
 
+    //untuk generate zoom code karena response data API tidak ada zoom code
     function generateZoomCode() {
         const letters = 'abcdefghijklmnopqrstuvwxyz';
         let result = '';
@@ -69,6 +74,13 @@ const ManageBookingDetail = () => {
         result = result.match(/.{1,3}/g).join('-');
         return result;
     }
+
+    const handleButtonClick = (status) => {
+        setSelectedButton(status);
+    };
+
+    //untuk filter data yg ditampilkan online/offline
+    const filteredDatas = customers.filter(data => data.class_package?.class.class_type === selectedButton);
     
     return (
         <>
@@ -83,39 +95,56 @@ const ManageBookingDetail = () => {
                     <h3 style={{ color: "var(--primary-900)" }}>Order Customer</h3>
                     <div className='d-flex justify-content-between'>
                         <h5>Here Customer Data</h5>
-                        <h5 style={{color: 'var(--Neutral-White-800)'}}>Showing 5 from {customers.length} data</h5>
+                        <div>
+                            <Button
+                                className='me-2 shadow'
+                                onClick={() => handleButtonClick('online')}
+                                style={{ backgroundColor: selectedButton === 'online' ? 'var(--Warning-500)' : 'white', color: 'var(--Warning-900)', border: 'none' }}
+                            >
+                                Online
+                            </Button>
+                            <Button
+                                className="shadow"
+                                onClick={() => handleButtonClick('offline')}
+                                style={{ backgroundColor: selectedButton === 'offline' ? 'var(--Warning-500)' : 'white', color: 'var(--Warning-900)', border: 'none' }}
+                            >
+                                Offline
+                            </Button>
+                        </div>
                     </div>
                 </div>
                 <Row>
-                    <Col md={6} className="px-2 py-3 shadow">
+                    <Col md={12} className="px-2 py-3 shadow">
                         <DetailBooking 
                             bookingType={booking.class_package?.period}
                             periode={booking.class_package?.class.started_at ? formatDate(booking.class_package.class.started_at, 'id-ID') : ''}
                             session={booking.class_package?.class.started_at ? formatTime(booking.class_package.class.started_at) : ''}
-                            zoomCode={
-                                booking.class_package?.class.class_type === 'online'? generateZoomCode().toUpperCase(): '-'
-                            }
+                            zoomCode={generateZoomCode().toUpperCase()}
+                            location={booking.class_package?.class.location.address}
                             classType={booking.class_package?.class.class_type + " class"}
                             descClass={
                                 booking.class_package?.class.class_type === 'online'? 'Private zoom with mentor': 'offline class with trainer'
                             }
                             status={booking.status}
-                            image={booking.user?.profile_picture}
+                            image={`http://18.141.56.154:8000/${booking.user?.profile_picture}`}
                         />
                     </Col>
-                    <Col md={6} className="px-5 py-3 shadow">
-                        <h2 className='mb-3' style={{ color: "var(--Neutral-Black-100)" }}>Customer Data</h2>
+                    <Col md={12} className="mt-5 py-3">
+                        <div className='d-flex justify-content-between'>
+                            <h2 className='mb-3' style={{ color: "var(--Neutral-Black-100)" }}>Customer Data</h2>
+                            <h5 style={{color: 'var(--Neutral-White-800)'}}>Showing {Math.min(5, filteredDatas.length)} from {filteredDatas.length} data</h5>
+                        </div>
                         {
-                            customers.length > 0 ? (
-                                customers.slice(0, 5).map((customer, index) => { 
+                            filteredDatas.length > 0 ? (
+                                filteredDatas.slice(0, 5).map((customer, index) => { 
                                     return (
                                         <CardCustomerBooking 
                                             key={index}
                                             id={customer.id}
                                             name={customer.user.name}
-                                            date={formatDate(customer.class_package.class.started_at, 'en-US')}
+                                            date={formatDate(customer.class_package?.class?.started_at, 'en-US')}
                                             status={customer.status}
-                                            image={customer.profile_picture}
+                                            image={`http://18.141.56.154:8000/${customer.user.profile_picture}`}
                                         />
                                     );
                                 })                                  
