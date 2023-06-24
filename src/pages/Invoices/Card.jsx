b578f5daa037d12a6f22a8d8112b6711c9e52f25
riskaamelia-wd <95574503+riskaamelia-wd@users.noreply.github.com>
2023-06-24 23:55:55 +0700

    add : desc, asc, pagination, etc

--- src/pages/Invoices/Card.jsx
+++ src/pages/Invoices/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({img, text, number}) => {
                 <p className='m-0 fw-bold' style={{fontSize:'24px'}}>{number}</p>
                 <p className='m-0' style={{color:'var(--Neutral-White-800)', fontSize:'20px'}}>{text}</p>
             </div>
-            <div className='p-3 rounded-circle border-3 border'>
+            <div className='text-center pt-2 rounded-circle border-3 border'>
                 <img src={img} alt="" />
             </div>
         </div>
